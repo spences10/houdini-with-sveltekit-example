5fd2a1e42cc0d66fca4acf95b64195d771059df0
spences10 <spences10apps@gmail.com>
2024-03-31 10:50:46 +0100

    refactor: :recycle: use new SvelteKit skeleton

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
-import { sveltekit } from '@sveltejs/kit/vite'
-import houdini from 'houdini/vite'
-import { defineConfig } from 'vite'
+import { sveltekit } from '@sveltejs/kit/vite';
+import houdini from 'houdini/vite';
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
 	plugins: [houdini(), sveltekit()],
-})
+	test: {
+		include: ['src/**/*.{test,spec}.{js,ts}']
+	}
+});
